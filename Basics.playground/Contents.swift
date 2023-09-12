143808809494a1ac4f1edea22e6e8604b2cf159e
@kaman4 <kaman@tekion.com>
2023-09-12 22:48:57 +0530

    Initial Commit

---
+++ Basics.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var num = 10
+var siteName = "https://www.google.com"
+//print(siteName)
+//siteName = "https://www.offensivesecurity.com"
+//print(siteName)
+var letter: Character = "s"
+print(letter)
+
