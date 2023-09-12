3f542b7d35f73f8f4ade5f5ebc83960b4b1e217e
@kaman4 <kaman@tekion.com>
2023-09-12 23:14:54 +0530

    BinarySearch

--- Basics.playground/Contents.swift
+++ Basics.playground/Contents.swift
@@ -1,10 +1,28 @@
 import UIKit
 
-var num = 10
-var siteName = "https://www.google.com"
-//print(siteName)
-//siteName = "https://www.offensivesecurity.com"
-//print(siteName)
-var letter: Character = "s"
-print(letter)
+func binarySearch(arr: [Int], target: Int) -> Int {
+    var start = 0
+    var end = arr.count - 1
+    
+    while(start <= end){
+        let mid = start + (end - start)/2
+        
+        if (arr[mid] == target){
+            return mid
+        }
+        else if (arr[mid] < target){
+            start = mid + 1
+        }
+        else {
+            end = mid - 1
+        }
+    }
+    
+    return -1
+}
 
+let arr = [2, 3, 4, 6, 7, 8, 10]
+let target = 7
+let result = binarySearch(arr: arr, target: target)
+
+print("IDX: \(result)")
